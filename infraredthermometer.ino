e0705cd76b40582c7b0a0f91736b18149d8d0e97
F Varas <tatico@gmail.com>
2015-06-28 15:39:33 +0200

    text color changes depending on temp

--- infraredthermometer.ino
+++ infraredthermometer.ino
@@ -6,7 +6,6 @@
 #define TFT_RST    9  // you can also connect this to the Arduino reset 
 #define TFT_DC     8
 #define Landscape  1
-// Color definitions
 #define BLACK    0x0000
 #define BLUE     0x001F
 #define RED      0xF800
@@ -16,32 +15,51 @@
 #define YELLOW   0xFFE0 
 #define WHITE    0xFFFF
 
-// IR part
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 float temp;
 
 void setup() {
-  mlx.begin();  
+  mlx.begin();
+  Serial.begin(9600);  
  // Use this initializer if you're using a 1.8" TFT
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
   uint16_t time = millis();
+  time = millis() - time;
   tft.fillScreen(ST7735_BLACK);
   tft.setRotation (Landscape);
-  tft.print("You are getting nerdier every second you stare at this screen...");
+  tft.println("You are getting nerdier every second you stare at this screen...");
   delay(300);
 }
 
 void loop() {
-   tft.setCursor(45, 50);
+   tft.setTextSize(1);
+   tft.setTextColor(WHITE);
+   tft.setCursor(35,15);
+   tft.print("uptime: ");
+   tft.print(millis() / 1000);
+   tft.print("s");
+   tft.setCursor(35, 50);
    tft.setTextSize(3);
    temp=mlx.readObjectTempC();
-   //tft.print(mlx.readObjectTempC());
+   if (temp < 5) {
+     tft.setTextColor(BLUE);
+   }
+   else if (temp > 30) {
+     tft.setTextColor(RED);
+   }
+   //tft.setTextColor(MAGENTA);
    tft.print(temp);
+   //tft.println(mlx.readObjectTempC());
+   //Serial.println(mlx.readObjectTempC());
+   tft.setCursor(35,75);
+   tft.setTextSize(1);
+   tft.println("Celsius degrees");
    delay(500);
    tft.fillScreen(ST7735_BLACK);
 }
